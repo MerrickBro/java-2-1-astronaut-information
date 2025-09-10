c317d911b32885264bed83917241b668009ec73c
Merrick Bublitz <merrickab90@gmail.com>
2025-09-10 08:30:11 -0400

    Completed
    Even added a needed "identity" string, not every astronaut is gonna be a "he", I think.

--- AstronautInformation.java
+++ AstronautInformation.java
@@ -2,11 +2,19 @@ public class AstronautInformation {
     
     public static void main(String[] args) {
         // Create the variables below
-
+        String name;
+        String identity;
+        Integer age;
+        String rank;
+        double time;
         // Store information for the variables below
-
+        name = "Buzz Aldrin";
+        identity = "He";
+        age = 90;
+        rank = "Colonel";
+        time = 12.0784;
         // Display the message below
-        
+        System.out.println(name + " is " + age + " years old.");
+        System.out.println(identity + " is a " + rank + " and has spent " + time + " days in space.");
     }
-    
 }
